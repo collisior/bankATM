9c4b611a1ea44a8bd1b6a4e977ce437a37c3849b
Camilla Satte <camillasatte@gmail.com>
2020-04-30 22:43:51 -0400

    OpenClose interface included

---
+++ bankATM/src/account/OpenClose.java
@@ -0,0 +1,13 @@
+package account;
+
+import java.sql.SQLException;
+
+import bankATM.*;
+
+public interface OpenClose {
+	
+	public abstract void open(Client client) throws SQLException;
+
+	public abstract void close(Client client) throws SQLException;;
+	
+}
